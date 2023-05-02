9aa479145f54cc6a7c481553c820fdf2e41258f1
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-05-02 21:32:05 +0600

    user login has modify successfully and creating user

--- src/Home/Login/Login.jsx
+++ src/Home/Login/Login.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../provider/AuthProvider';
 
 
 const Login = () => {
+    const { loginUser } = useContext(AuthContext);
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+
+        loginUser(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                form.reset();
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
     return (
         <Container className='w-50 mx-auto shadow-lg p-3 mb-5 bg-white rounded mt-3 px-5 py-5 '>
             <h2>Please Login</h2>
-            <Form>
+            <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" name='email' required placeholder="Enter email" />
